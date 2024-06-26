401ea404ae90f62126b6d40fb9209228f3e44eaf
tjhv10 <achiyabennatan@gmail.com>
2024-06-26 12:29:06 +0300

    update

--- mincover(3B).py
+++ mincover(3B).py
@@ -1,16 +1,19 @@
 import subprocess, sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxpy"], stdout=subprocess.DEVNULL)
-subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxopt"], stdout=subprocess.DEVNULL)
 
-import networkx as nx, cvxpy as cp, numpy as np, matplotlib.pyplot as plt
+subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxpy"], stdout=subprocess.DEVNULL)
+
+import networkx as nx
 from time import perf_counter
 
 def mincover(graph: nx.Graph)->int:
     n = len(graph.nodes())
+    if n==0:
+        return 0
+    import cvxpy as cp
     x = cp.Variable(n, boolean=True)
     constraints = []
     for i, j in graph.edges():
-        constraints.append(x[i] + x[j] >= 1)
+        constraints.append(x[i] + x[j-1] >= 1)
     problem = cp.Problem(cp.Minimize(cp.sum(x)), constraints)
     try:
         problem.solve(solver=cp.GLPK_MI)
@@ -26,6 +29,4 @@ def measure_time(func, *args, **kwargs):
     return end - start, res
 
 if __name__ == '__main__':
-    edges=eval(input())
-    graph = nx.Graph(edges)
-    print(mincover((graph)))
+    print(mincover((nx.Graph(eval(input())))))
